1155dd9181486087a337bade4be7c10a37ce9b9c
WhiiteRose <tom.roger@epitech.eu>
2025-08-26 00:55:02 +0200

    feat: :sparkles: Message Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import NavBar from "./components/NavBar";
 import HeroSection from "./sections/HeroSection";
+import MessageSection from "./sections/MessageSection";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -11,6 +12,7 @@ const App = () => {
     <main>
       <NavBar />
       <HeroSection />
+      <MessageSection />
       <div className="h-dvh border border-red-500"></div>
     </main>
   );
