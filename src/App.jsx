15384a39bd5d8915aa0e24899e2cc9b48b3204c1
WhiiteRose <tom.roger@epitech.eu>
2025-08-25 22:32:01 +0200

    feat: :sparkles: Gsap plugin features with transition and scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import NavBar from "./components/NavBar";
 import HeroSection from "./sections/HeroSection";
 
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
 
@@ -8,6 +11,7 @@ const App = () => {
     <main>
       <NavBar />
       <HeroSection />
+      <div className="h-dvh border border-red-500"></div>
     </main>
   );
 };
