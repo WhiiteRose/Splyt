ccdab1aac3d7d531abd271594f55649c65072db4
WhiiteRose <tom.roger@epitech.eu>
2025-08-28 00:23:07 +0200

    feat: :sparkles: FlavourSection with slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import NavBar from "./components/NavBar";
+import FlavourSection from "./sections/FlavourSection";
 import HeroSection from "./sections/HeroSection";
 import MessageSection from "./sections/MessageSection";
 
@@ -13,6 +14,7 @@ const App = () => {
       <NavBar />
       <HeroSection />
       <MessageSection />
+      <FlavourSection />
       <div className="h-dvh border border-red-500"></div>
     </main>
   );
